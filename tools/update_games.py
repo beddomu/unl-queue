55b05b968fac79fb2e4e2631230c826f22a6dd3c
beddomu <giacomo.cossu1998@gmail.com>
2022-04-21 18:53:17 +0200

    Update update_games.py

--- tools/update_games.py
+++ tools/update_games.py
@@ -1,12 +1,17 @@
+import collections
 import os
 import json
 
 
-games = []
+games = {}
 for fn in os.listdir("C:\\DATA\\games"):
         with open(f'C:\\DATA\\games\\{fn}', 'r') as file:
-            game = json.load(file)
-        games.insert(0, game)
+            game_file = json.load(file)
+            games[fn] = {}
+            games[fn]["game_id"] = game_file['info']['gameId']
+            games[fn]["game_end"] = game_file['info']['gameEndTimestamp']
+
+res = collections.OrderedDict(sorted(games.items(), key=lambda t:t[1]["game_end"], reverse=True))
         
 with open('..\\unlqueue.xyz\\games\\games.json' , 'w') as file:
-    json.dump(games, file)
+    json.dump(res, file)
