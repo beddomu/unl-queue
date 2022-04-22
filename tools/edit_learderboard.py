b2d47112806f916855e5022bc2d34f37630cb941
beddomu <giacomo.cossu1998@gmail.com>
2022-04-23 00:49:30 +0200

    Update edit_learderboard.py

--- tools/edit_learderboard.py
+++ tools/edit_learderboard.py
@@ -4,9 +4,7 @@ with open('C:\\DATA\\unlq.json', 'r') as file:
     unlq = json.load(file)
     
 for p in unlq['players']:
-    if "unp" not in unlq['players'][p].keys():
-        unlq['players'][p]['unp'] = 0
-        print(p)
+    unlq['players'][p]['banned_until'] = 0
         
 
 with open('C:\\DATA\\unlq.json', 'w') as unlq_file:
