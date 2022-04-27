0d28942102fab9d1c51334e783ce26159621069d
beddomu <giacomo.cossu1998@gmail.com>
2022-04-28 01:59:00 +0200

    Update get_match_history.py

--- utils/get_match_history.py
+++ utils/get_match_history.py
@@ -1,3 +1,4 @@
+from pprint import pp
 import urllib
 import os
 import json
@@ -6,6 +7,7 @@ def get_match_history(puuid):
     try:
         with urllib.request.urlopen("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?start=0&count=20&api_key={}".format(puuid, os.getenv("RIOT_API_KEY"))) as file:
             match_history = json.loads(file.read().decode())
+            pp(match_history)
             return match_history
     except Exception as e:
         print('error trying to find puuid')
