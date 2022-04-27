ada650337862fe0fdccc42dd847161d7b4349392
beddomu <giacomo.cossu1998@gmail.com>
2022-04-28 01:12:43 +0200

    Update report_game.py

--- utils/report_game.py
+++ utils/report_game.py
@@ -16,13 +16,14 @@ async def report_game(bot: commands.Bot, game_id, guild: discord.Guild):
         unlq_json = json.load(unlq_file)
 
     if int(game_id) not in unlq_json['games']:
-        print(0)
 
         with urllib.request.urlopen("https://europe.api.riotgames.com/lol/match/v5/matches/{}?api_key={}".format(game_id, os.getenv("RIOT_API_KEY"))) as game_json:
             game = json.loads(game_json.read().decode())
             lobby_id = game['info']['gameName']
-            print(lobby_id[9:])
+            print(lobby_id)
             if lobby_id[9:] in unlq_json['lobbies'].keys():
+                pp(unlq_json['lobbies'].keys())
+                print("=============================")
                 print(1)
                 channel = await bot.fetch_channel(int(os.getenv("LIVE")))
                 try:
