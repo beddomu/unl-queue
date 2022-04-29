1cdac5dc28c693fe7f6da138392ac576273b6b29
beddomu <giacomo.cossu1998@gmail.com>
2022-04-29 20:01:13 +0200

    Fix

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import json
 import os
 import random
@@ -8,6 +9,7 @@ import discord
 from discord.ext import commands, tasks
 from discord import app_commands
 import dotenv
+import pytz
 from utils.find_summoner import find_summoner
 from utils.get_match_history import get_match_history
 from utils.report_game import report_game
@@ -27,7 +29,26 @@ class MyBot(commands.Bot):
             
     @tasks.loop(minutes=0.25)
     async def background_task(self):
-        pass
+        with open('C:\\DATA\\unlq.json', 'r') as file:
+            unlq = json.load(file)
+        if unlq['dev_mode'] == True:
+            now = datetime.datetime.fromtimestamp(datetime.datetime.now().timestamp(), pytz.timezone('Europe/London'))
+            if 0 <= now.weekday() <= 4:
+                if datetime.time(19) <= now.time() <= datetime.time(22):
+                    print("Opening queue...")
+
+                    unlq['dev_mode'] = False
+
+                    with open('C:\\DATA\\unlq.json', 'w') as unlq_file:
+                        json.dump(unlq, unlq_file)
+                    guild = await self._bot.fetch_guild(int(os.getenv("SERVER_ID")))
+                    role = discord.utils.get(guild.roles, id = 676740137815900160)
+                    channel = await self._bot.fetch_channel(int(os.getenv("QUEUE")))
+                    await channel.set_permissions(role, read_messages=True)
+                    channel = await self._bot.fetch_channel(int(os.getenv("LIVE")))
+                    await channel.set_permissions(role, read_messages=True)
+                    self.queue.devmode = False
+                    await self.queue.new_lobby()
         
     async def on_ready(self):
         self.background_task.start()
