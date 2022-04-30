f1518ba5de39d47fc9402f8018cc2d99d81d881a
beddomu <giacomo.cossu1998@gmail.com>
2022-04-30 18:38:17 +0200

    Update pay.py

--- classes/views/pay.py
+++ classes/views/pay.py
@@ -40,7 +40,10 @@ class Pay(discord.ui.Modal):
                 unlq = json.load(file)
                 
             if unlq['players'][str(self.sender.id)]['unp'] >= amount:
+                pp(unlq['players'][str(self.sender.id)]['unp'])
+                pp(amount)
                 unlq['players'][str(self.sender.id)]['unp'] -= amount
+                pp(unlq['players'][str(self.sender.id)]['unp'])
                 
                 try:
                     unlq['players'][str(self.receiver.id)]['unp'] += amount
