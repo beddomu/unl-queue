7fcd6c946a28fb78333e247c8ba58122ff0c21b1
beddomu <giacomo.cossu1998@gmail.com>
2022-09-26 20:07:24 +0200

    yes

--- dev_bot.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-import pytz
-
-now = datetime.datetime.fromtimestamp(datetime.datetime.now().timestamp(), pytz.timezone('Europe/London'))
-if 0 <= now.weekday() <= 4:
-    print("it's a weekday")
-    if datetime.time(19) <= now.time() <= datetime.time(22):
-        print("and it's in range")
---
+++ lcu/dev_bot.py
@@ -0,0 +1,45 @@
+from pprint import pp, pprint
+import requests
+import json
+import lockfile
+
+
+class Lobby:
+    def __init__(self, name: str, password=None, team_size: int = 5, map_id: int = 11, mutator_id: int = 2):
+        self.auth = lockfile.auth
+        self.port = lockfile.port
+        self.name = name
+        self.password = password
+        self.team_size = team_size
+        self.map_id = map_id
+        self.mutator_id = mutator_id
+        self.url = 'https://127.0.0.1:{}/lol-lobby/v2/lobby'.format(self.port)
+        self.payload = {
+            "customGameLobby": {
+                "configuration": {
+                    "gameMode": "CLASSIC",
+                    "gameMutator": "",
+                    "gameServerRegion": "EUW",
+                    "mapId": self.map_id,
+                    "mutators": {
+                        "id": self.mutator_id,
+                    },
+                    "spectatorPolicy": "AllAllowed",
+                    "teamSize": self.team_size
+                },
+                "lobbyName": "UNLQ ID: {}".format(self.name),
+                "lobbyPassword": "{}".format(password)
+            },
+            "isCustom": True
+        }
+        self.headers = {'accept': 'application/json', 'Authorization': 'Basic {}'.format(self.auth), 'Content-Type': 'application/json'}
+
+    def create(self):
+        r = requests.post(self.url, data=json.dumps(self.payload),headers=self.headers, verify=False)
+        if r.status_code == 200:
+            print("Lobby created successfully")
+        else:
+            pp(r.json())
+
+lobby = Lobby('test', None, 5, 11, 13)
+lobby.create()
